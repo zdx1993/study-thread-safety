3ed5eeaecf5e7c7f8ef024b0cd759cd97fc0cd4a
dx <505707606@qq.com>
2019-04-21 18:42:03 +0800

    存在线程安全的代码展示

--- src/main/java/dx/demo/Count.java
+++ src/main/java/dx/demo/Count.java
@@ -1,12 +1,24 @@
 package dx.demo;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Count {
-    private int num;
+
+    private long num;
 
     public void count() {
-        for (int i = 1; i <= 10; i++) {
-            num += i;
+        ReentrantLock reentrantLock = new ReentrantLock();
+        reentrantLock.lock();
+        try {
+            for (int i = 1; i <= 1000000; i++) {
+                num += i;
+            }
+            System.out.println(Thread.currentThread().getName() + "-" + num);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(Thread.currentThread().getName() + "的锁释放了");
+            reentrantLock.unlock();
         }
-        System.out.println(Thread.currentThread().getName() + "-" + num);
     }
 }  
